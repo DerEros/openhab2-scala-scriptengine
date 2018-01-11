589e81f13d095953308d35153ce151acef748bf8
Eros Candelaresi <eros@hpe.com>
2018-01-11 13:23:53 +0000

    Tried different approaches to get Ammonite or at least the script engine to run in OSGI - failed!

--- src/main/scala/de/erna/Main.scala
+++ src/main/scala/de/erna/Main.scala
@@ -1,6 +1,28 @@
 package de.erna
 
-object Main extends App {
+import javax.script.{Bindings, ScriptContext}
+
+import org.slf4s.Logging
+
+import scala.tools.nsc.interpreter.Scripted
+
+object Main extends App with Logging {
+  def say(msg: String): Unit = {
+    System.out.println(msg)
+  }
+
   System.out.println("Hello App")
 
+//  ammonite.Main().runCode("println(\"Hello Ammonite\")")
+
+  val scripted = Scripted()
+  val context = scripted.compileContext
+  val bindings = context.getBindings(ScriptContext.ENGINE_SCOPE)
+
+  bindings.put("say", say _)
+  bindings.put("msg", "mymsg")
+
+  val compiledScript = scripted.compile("throw new Exception(msg.asInstanceOf[String])", context)
+
+  compiledScript.eval()
 }
--- src/main/scala/de/erna/ScalaEngineActivator.scala
+++ src/main/scala/de/erna/ScalaEngineActivator.scala
@@ -1,7 +1,13 @@
 package de.erna
 
+import javax.script.ScriptContext
+
+import de.erna.Main.say
 import org.osgi.framework.{BundleActivator, BundleContext}
 import org.slf4s.Logging
+
+import scala.tools.nsc.Settings
+import scala.tools.nsc.interpreter.Scripted
 
 class ScalaEngineActivator extends BundleActivator with Logging {
   var directoryMonitor: DirectoryMonitor = _
@@ -10,6 +16,22 @@ class ScalaEngineActivator extends BundleActivator with Logging {
     log.info("Starting bundle")
 
     directoryMonitor = new DirectoryMonitor("/tmp", () => _, () => _, () => _)
+
+    val settings = new Settings()
+    settings.classpath.value = System.getProperty("java.class.path")
+    settings.usejavacp.value = true
+
+    val scripted = Scripted(settings = settings)
+    val context = scripted.compileContext
+    val bindings = context.getBindings(ScriptContext.ENGINE_SCOPE)
+
+    bindings.put("say", say _)
+    bindings.put("msg", "mymsg")
+
+    val compiledScript = scripted.compile("throw new Exception(msg.asInstanceOf[String])", context)
+
+    compiledScript.eval()
+
 
   }
 
